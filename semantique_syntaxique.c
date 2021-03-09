9e79aad5567781d25096e9fd92bd338022c5142f
hamzatamry <58271202+hamzatamry@users.noreply.github.com>
2021-03-09 16:06:30 +0100

    Ajout de structure d'analyseur_syntaxique

--- semantique_syntaxique.c
+++ semantique_syntaxique.c
@@ -1,30 +1,31 @@
 #include "semantique_syntaxique.h"
-
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 
-
-FILE *f;
-
-int current_size = 0;
-
+FILE* lexical_output;
 bool good = true;
-
-char ERR[][100] = {"erreur program","erreur constante", "erreur variable","erreur begin","End Erreur", "if erreur", "then erreur","While erreur",
-"Do erreur","Erreur READ","WRITE erreur","Point virgule erreur","point erreur","double points erreur","plus erreur","monis erreur","multiplication erreur",
-"division erreur", "virgule erreur", "egale erreur", "affectation erreur", "inferieur erreur", "inferieur ou egale erreur", "superieur erreur", "superieur ou egale erreur",
-"different erreur","parenthese ouvrante erreur", "parenthese fermante erreur", "debut commentaire erreur", "fin commentaire erreur", "accolade ouvrante erreur",
-"accolade fermante erreur", "fin de fichier erreur", "identificateur erreur", "numero erreur","else erreur", "until erreur", "repeat erreur", "for erreur",
-"downto erreur", "case erreur", "of erreur", "into erreur", "erreur debut programme", "instruction erreur", "facteur erreur", "pour erreur", "fichier vide",
-"cas erreur","erreur doublons","erreur modification constante", "erreur identificateur non declare", "erreur identificateur du programme non autorise", "erreur declaration type" ,"erreur is or :"};
+char ERR[][100] = {
+    "erreur program","erreur constante", "erreur variable","erreur begin","End Erreur", "if erreur", "then erreur","While erreur",
+    "Do erreur","Erreur READ","WRITE erreur","Point virgule erreur","point erreur","double points erreur","plus erreur","monis erreur","multiplication erreur",
+    "division erreur", "virgule erreur", "egale erreur", "affectation erreur", "inferieur erreur", "inferieur ou egale erreur", "superieur erreur", "superieur ou egale erreur",
+    "different erreur","parenthese ouvrante erreur", "parenthese fermante erreur", "debut commentaire erreur", "fin commentaire erreur", "accolade ouvrante erreur",
+    "accolade fermante erreur", "fin de fichier erreur", "identificateur erreur", "numero erreur","else erreur", "until erreur", "repeat erreur", "for erreur",
+    "downto erreur", "case erreur", "of erreur", "into erreur", "erreur debut programme", "instruction erreur", "facteur erreur", "pour erreur", "fichier vide",
+    "cas erreur","erreur doublons","erreur modification constante", "erreur identificateur non declare", "erreur identificateur du programme non autorise", "erreur declaration type" ,"erreur is or :"
+};
 
 
 void ouvrir_fichier(char nom[20])
 {
-    f = fopen(nom,"r");
-    if(f == NULL)
+    lexical_output = fopen(nom,"r");
+    
+    if (lexical_output == NULL) 
+    {
         printf("erreur ouverture fichier");
+        exit(-1);
+    }
 
 }
 
@@ -32,14 +33,14 @@ void Sym_Suiv()
 {
     char c;
     int i = 0;
-    strcpy(previous,SYM_COUR.NOM);
-    while((c = fgetc(f)) != ' ')
+
+    while((c = fgetc(lexical_output)) != ' ')
     {
         SYM_COUR.NOM[i] = c;
         i++;
     }
     SYM_COUR.NOM[i] = '\0';
-    fscanf(f,"%d", &(SYM_COUR.CODE));
+    fscanf(lexical_output,"%d", &(SYM_COUR.CODE));
 }
 
 void Test_Symbole(CODES_LEX lex,ERREURS err)
@@ -53,372 +54,13 @@ void Test_Symbole(CODES_LEX lex,ERREURS err)
         ERREUR(err);
 }
 
-void PROGRAM(){
+void PROGRAM()
+{
 
-};
-void INSTRUCTIONS(){
-  
-};
-void INSTRUCTION(){
-
-};
-void INST1(){
-
-};
-void ACC_O(){
-
-};
-void ACC_F(){
-
-};
-void AFFECTATION(){
-
-};
-void APPEL_FONCTION(){
-
-};
-void BOUCLE(){
-
-};
-void RETURN(){
-
-};
-void VAR_DECLARATION(){
-    if( SYM_COUR.CODE == CONST_TOKEN || SYM_COUR.CODE == LET_TOKEN || SYM_COUR.CODE == ID_TOKEN)
-        VARS1();
-    else 
-        Test_Symbole(PV_TOKEN,PV_ERR);                // cas où VAR_DECLARATION == epsilon
-    };
-
-void FONCTION(){
-
-};
-void CONTROLE(){
-
-};
-void EXCEPTION(){
-
-};
-void FILEHANDLING(){
-
-};
-void AFFECTATION1(){
-
-};
-void SYMBOLE_AFF(){
-
-};
-void ID(){
-
-};
-void EXPRESSION(){
-
-};
-void EG(){
-
-};
-void TERM(){
-
-};
-void EX1(){
-
-};
-void OPPERATEURADD(){
-
-};
-void VARS1(){
-    switch (SYM_COUR.CODE)
-    {
-    case CONST_TOKEN:
-        Sym_Suiv();
-        TYPE();             // ICI je pense c'est un terminal (make sure to call sym_suiv)
-        IDS_CONST();
-        break;
-    case LET_TOKEN:
-        Sym_Suiv();
-        JS_IDS();
-        break;
-    case ID_TOKEN:
-        VARS2();
-        break;
-    default:
-        break;
-    }
-};
-
-void TYPE(){
-    switch(SYM_COUR.CODE){
-        case INT_TOKEN:
-            break;
-        case INTEGER_TOKEN:
-            break;
-        case NUMBER_TOKEN:
-            break;
-        case FLOAT_TOKEN:
-            break;
-        case STR_TOKEN:
-            break;
-        case LONG_TOKEN:
-            break;
-        case DOUBLE_TOKEN:
-            break;
-        case SHORT_TOKEN:
-            break;
-        case UNSIGNED_TOKEN:               // ajouter les autres types apres ?
-            break;
-        case STRING_TOKEN:
-            break;
-        case CHAR_TOKEN:
-            break;
-        case BOOL_TOKEN:
-            break;
-        case BOOLEAN_TOKEN:
-            break;
-        default: 
-            ERREUR(TYPE_ERR);
-    }
-    Sym_Suiv();                           // à verifier si jamais il'ya une erreur
-};
-
-void IDS_CONST(){
-    Test_Symbole(ID_TOKEN,ID_ERR);
-    AFFECTATION1();
-    IDS1();
-};
-
-void JS_IDS(){
-    Test_Symbole(ID_TOKEN,ID_ERR);
-    Test_Symbole(PTS_TOKEN,PTS_ERR);
-    TYPE();
-    OPT();
-    JS1();
-};
-void VARS2(){
-    Sym_Suiv();
-    Test_Symbole(ID_TOKEN,ID_ERR);
-    VARS_TYPE();
-    OPT();
-    VARS3();
-};
-void VARS_TYPE(){
-    switch (SYM_COUR.CODE)
-    {
-    case PTS_TOKEN:
-        Sym_Suiv();
-        TYPE();
-        break;
-    case IS_TOKEN:
-         Sym_Suiv();
-        TYPE();
-        break;
-    default:
-        ERREUR(VARS_TYPE_ERR);
-        break;
-    }
-};
-
-void OPT(){
-    if(SYM_COUR.CODE == PTS_TOKEN || SYM_COUR.CODE == EG_TOKEN || SYM_COUR.CODE == AFFECARROW_TOKEN)
-        AFFECTATION1();
-    else
-    {
-        switch (SYM_COUR.CODE)
-        {
-        case PTS_TOKEN:
-            VARS3();
-            break;
-        case PV_TOKEN:                    
-            JS1();
-            break;
-        default:
-            return;
-        }
-      
-    }
-       
-};
-void VARS3(){
-    if( SYM_COUR.CODE == VIR_TOKEN)
-    {
-        Sym_Suiv();
-        VARS2();
-    }
-    else{
-        Test_Symbole(PV_TOKEN,PV_ERR);              // check the followings of VARS3
-        return;
-    }
-};
-
-void IDS(){
-    // Test_Symbole(ID_TOKEN,ID_ERR);
-    // OPT();
-    // IDS2();
-
-    // à supprimer ?
-};
-void IDS1(){
-    if (SYM_COUR.CODE == VIR_TOKEN)
-    {
-        Sym_Suiv();
-        IDS_CONST();
-    }
-    else 
-    {
-        Test_Symbole(PV_TOKEN,PV_ERR);              // check the followings of IDS1
-    }
-};
-void JS1(){
-    if (SYM_COUR.CODE == VIR_TOKEN){
-        Sym_Suiv();
-        JS_IDS();
-    }
-    else{
-        Test_Symbole(PV_TOKEN,PV_ERR);
-        return;
-    }
-       
-};
-void IDS2(){
-    // à supprimer ?
-};
-void IF_STMT(){
-
-};
-void COND(){
-
-};
-void IF_STMT_SYMB(){
-
-};
-void SUITE1(){
-
-};
-void END(){
-
-};
-void SWITCH(){
-
-};
-void SWITCH_IF(){
-
-};
-void STATEMENT(){
-
-};
-void SUITE_IF(){
-
-};
-void ELIF_STMT(){
-
-};
-void ELSE_BLOCK(){
-
-};
-void ELIF_SUITE(){
-
-};
-void SHORTHAND(){
-
-};
-void DOWHILELOOP_STATEMENT(){
-
-};
-void STATEMENTS(){
-
-};
-void CONDITION(){
-
-};
-void FORLOOP_STATEMENT(){
-
-};
-void A_F(){
-
-};
-void B_F(){
-
-};
-void AF1(){
-
-};
-void NOMBRE(){
-
-};
-void B_F1(){
-
-};
-void WHILELOOP_STATEMENT(){
-
-};
-void A_W(){
-
-};
-void B_W(){
-
-};
-void PARAMETERS(){
-
-};
-void FONCTION1(){
-
-};
-void PARAMETER(){
-
-};
-void OPPERATEURSPECIAUX(){
-
-};
-void COMPAR(){
-
-};
-void OPPERATEURMULT(){
-
-};
-void STRING(){
-
-};
-
-
-
-
-
-
-
-
+}
 
 void ERREUR(ERREURS e)
 {
     good = false;
    printf("%s\n",ERR[e]);
-}
-
-void addID(TSYM type)
-{
-    for(int i = 0; i < current_size; i++)
-    {
-        if(strcmp(previous,TAB_IDFS[i].NOM) == 0)
-        {
-            ERREUR(DOUBLONS_ERR);
-            return;
-        }
-    }
-    strcpy(TAB_IDFS[current_size].NOM,previous);
-    TAB_IDFS[current_size].TIDF = type;
-    current_size++;
-}
-
-void semCheck(int accepted)
-{
-    for(int i = 0; i < current_size; i++)
-    {
-        if(strcmp(TAB_IDFS[i].NOM,previous) == 0)
-        {
-            if(TAB_IDFS[i].TIDF == TCONST && !accepted)
-                ERREUR(CONST_MODIF_ERR);
-            else if(TAB_IDFS[i].TIDF == TPROG)
-                ERREUR(PRO_ID_ERR);
-            return;
-        }
-    }
-    ERREUR(UNDECLARED_ERR);
 }
--- semantique_syntaxique.h
+++ semantique_syntaxique.h
@@ -1,164 +1,49 @@
 #ifndef SEMANTIQUE_SYNTAXIQUE_H
 #define SEMANTIQUE_SYNTAXIQUE_H
+
 #include <stdio.h>
 #include <stdbool.h>
 
-
-extern FILE *f;
-extern bool good;  // variale globale indiquant si l'analyseur syntaxique valide ou pas le fichier
-
-typedef enum {
-    INT_TOKEN, INTEGER_TOKEN, NUMBER_TOKEN, FLOAT_TOKEN, CHAR_TOKEN, STRING_TOKEN, STR_TOKEN, LONG_TOKEN,
-    DOUBLE_TOKEN, SHORT_TOKEN, UNSIGNED_TOKEN, BOOLEAN_TOKEN, BOOL_TOKEN, LET_TOKEN, $_TOKEN, BYTE_TOKEN, AUTO_TOKEN,
+typedef enum 
+{
+    INT_TOKEN, INTEGER_TOKEN, NUMBER_TOKEN, FLOAT_TOKEN, CHAR_TOKEN, STRING_TOKEN, STR_TOKEN, LONG_TOKEN, DOUBLE_TOKEN, SHORT_TOKEN,
+    UNSIGNED_TOKEN, BOOLEAN_TOKEN, BOOL_TOKEN, LET_TOKEN, $_TOKEN, BYTE_TOKEN, AUTO_TOKEN, VOID_TOKEN, TRUE_TOKEN, FALSE_TOKEN, 
     PROGRAM_TOKEN, CONST_TOKEN, VAR_TOKEN, BEGIN_TOKEN, END_TOKEN, IF_TOKEN, THEN_TOKEN, WHILE_TOKEN, DO_TOKEN, READ_TOKEN, WRITE_TOKEN,
     SWITCH_TOKEN, GOTO_TOKEN, DEF_TOKEN, FUNCTION_TOKEN, PV_TOKEN, PT_TOKEN, PTS_TOKEN, PLUS_TOKEN, ADD_TOKEN, MOINS_TOKEN, MINUS_TOKEN, MULT_TOKEN,
     MULT1_TOKEN, DIV_TOKEN, DIV1_TOKEN, PER_TOKEN, MOD_TOKEN, MOD1_TOKEN, MODULO_TOKEN, VIR_TOKEN, EG_TOKEN, AFFECPASCAL_TOKEN, AFFECT_TOKEN,
-    AFFECARROW_TOKEN, INF_TOKEN, LSS_TOKEN, INFEG_TOKEN, LEQ_TOKEN, SUP_TOKEN, GTR_TOKEN, SUPEG_TOKEN, GEQ_TOKEN, EQU_TOKEN, EQU1_TOKEN, IS_TOKEN,
+    AFFECARROW_TOKEN, INF_TOKEN, LSS_TOKEN, INFEG_TOKEN, LEQ_TOKEN, SUP_TOKEN, GTR_TOKEN, SUPEG_TOKEN, GEQ_TOKEN, EQU_TOKEN, EQU1_TOKEN, IS_TOKEN, 
     TRIPPLEEQU_TOKEN, IN_TOKEN, PUISS_TOKEN, PUISSAFFEC_TOKEN, ADDAFFEC_TOKEN, MINUSAFFEC_TOKEN, MULTAFFEC_TOKEN, DIVAFFEC_TOKEN, MODAFFEC_TOKEN,
     BXORAFFEC_TOKEN, BANDAFFEC_TOKEN, BORAFFEC_TOKEN, INCREM_TOKEN, DECREM_TOKEN, DIFF_TOKEN, PO_TOKEN, PF_TOKEN, DC_TOKEN, FC_TOKEN, ACO_TOKEN, ACF_TOKEN,
     BXOR_TOKEN, TILD_TOKEN, LEFTSHIFT_TOKEN, RIGHTSHIFT_TOKEN, BAND_TOKEN, BOR_TOKEN, AND_TOKEN, AND1_TOKEN, OR_TOKEN, OR1_TOKEN, NOT_TOKEN, NOT1_TOKEN,
-    EOF_TOKEN, ID_TOKEN, NUM_TOKEN, ELSE_TOKEN, UNTIL_TOKEN, REPEAT_TOKEN, FOR_TOKEN, DOWNTO_TOKEN, CASE_TOKEN, OF_TOKEN, INTO_TOKEN , ERREUR_TOKEN
-}CODES_LEX;
+    QUOTE_TOKEN,SINGLEQUOTE_TOKEN, EOF_TOKEN, ID_TOKEN, NUM_TOKEN, ELSE_TOKEN, UNTIL_TOKEN, REPEAT_TOKEN, FOR_TOKEN, DOWNTO_TOKEN, CASE_TOKEN, OF_TOKEN,
+    INTO_TOKEN , RETURN_TOKEN, PRINT_TOKEN, PRINTF_TOKEN, SCANF_TOKEN, INPUT_TOKEN, LOG_TOKEN, FPRINTF_TOKEN, FSCANF_TOKEN, FREAD_TOKEN, FWRITE_TOKEN, 
+    PUTS_TOKEN, GETS_TOKEN, CALL_TOKEN, STRINGVAL_TOKEN, ERREUR_TOKEN
+} CODES_LEX;
 
-typedef struct {
+typedef struct 
+{
     CODES_LEX CODE;
     char NOM[20];
-}TSym_Cour;
+}   TSym_Cour;
 
 
 TSym_Cour SYM_COUR;
 
-typedef enum {
+typedef enum 
+{
     PROGRAM_ERR, CONST_ERR, VAR_ERR, BEGIN_ERR, END_ERR, IF_ERR, THEN_ERR, WHILE_ERR,
     DO_ERR, READ_ERR, WRITE_ERR, PV_ERR, PT_ERR, PTS_ERR, PLUS_ERR, MOINS_ERR, MULT_ERR, DIV_ERR,
     VIR_ERR, EG_ERR, AFF_ERR, INF_ERR, INFEG_ERR, SUP_ERR, SUPEG_ERR, DIFF_ERR, PO_ERR, PF_ERR,
     DC_ERR, FC_ERR, ACO_ERR, ACF_ERR, EOF_ERR, ID_ERR, NUM_ERR, ELSE_ERR, UNTIL_ERR, REPEAT_ERR, FOR_ERR,
     DOWNTO_ERR, CASE_ERR, OF_ERR, INTO_ERR ,CONST_VAR_BEGIN_ERR, INST_ERR, FACT_ERR, POUR_ERR, ERR_FICH_VID,
     CAS_ERR, DOUBLONS_ERR, CONST_MODIF_ERR, UNDECLARED_ERR, PRO_ID_ERR, TYPE_ERR, VARS_TYPE_ERR
-}ERREURS;
+}   ERREURS;
 
-typedef enum {
-    TPROG, TCONST, TVAR
-    }TSYM;
-
-typedef struct {
-    char NOM[20];
-    TSYM TIDF;
-} T_TAB_IDF;
-
-T_TAB_IDF TAB_IDFS[50];
-
-
-char previous[20];
-
-
+extern FILE* lexical_output;
+extern bool good;  // variale globale indiquant si l'analyseur syntaxique valide ou pas le fichier
 extern void ouvrir_fichier(char nom[20]);
 extern void Test_Symbole(CODES_LEX lex,ERREURS err);
 extern void Sym_Suiv();
-/* extern void PROGRAM();
-extern void BLOCK();
-extern void CONSTS();
-extern void VARS();
-extern void INSTS();
-extern void INST();
-extern void AFFEC();
-extern void SI();
-extern void TANTQUE();
-extern void ECRIRE();
-extern void LIRE();
-extern void COND();
-extern void EXPR();
-extern void TERM();
-extern void FACT();
-extern void SI();
-extern void REPETER();
-extern void POUR();
-extern void CAS();
-extern void CONTROL();
-extern void IF();
-extern void CASE();
-extern void BLOCK_CASE(); */
-
-extern void PROGRAM();
-extern void INSTRUCTIONS();
-extern void INSTRUCTION();
-extern void INST1();
-extern void ACC_O();
-extern void ACC_F();
-extern void AFFECTATION();
-extern void APPEL_FONCTION();
-extern void BOUCLE();
-extern void RETURN();
-extern void VAR_DECLARATION();
-extern void FONCTION();
-extern void CONTROLE();
-extern void EXCEPTION();
-extern void FILEHANDLING();
-extern void AFFECTATION1();
-extern void SYMBOLE_AFF();
-extern void ID();
-extern void EXPRESSION();
-extern void EG();
-extern void TERM();
-extern void EX1();
-extern void OPPERATEURADD();
-extern void VARS1();
-extern void TYPE();
-extern void IDS_CONST();
-extern void JS_IDS();
-extern void VARS2();
-extern void VARS_TYPE();
-extern void OPT();
-extern void VARS3();
-extern void IDS();
-extern void IDS1();
-extern void JS1();
-extern void IDS2();
-extern void IF_STMT();
-extern void COND();
-extern void IF_STMT_SYMB();
-extern void SUITE1();
-extern void END();
-extern void SWITCH();
-extern void SWITCH_IF();
-extern void STATEMENT();
-extern void SUITE_IF();
-extern void ELIF_STMT();
-extern void ELSE_BLOCK();
-extern void ELIF_SUITE();
-extern void SHORTHAND();
-extern void DOWHILELOOP_STATEMENT();
-extern void STATEMENTS();
-extern void CONDITION();
-extern void FORLOOP_STATEMENT();
-extern void A_F();
-extern void B_F();
-extern void AF1();
-extern void NOMBRE();
-extern void B_F1();
-extern void WHILELOOP_STATEMENT();
-extern void A_W();
-extern void B_W();
-extern void PARAMETERS();
-extern void FONCTION1();
-extern void PARAMETER();
-extern void OPPERATEURSPECIAUX();
-extern void COMPAR();
-extern void OPPERATEURMULT();
-extern void STRING();
-
-
-
-
-
-
-
-
 extern void ERREUR(ERREURS e);
-
-extern void addID(TSYM type);
-extern void semCheck(int accepted);
-
-
 
 #endif
