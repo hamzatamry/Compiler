a8292e3bbb190c086a0c6e89f557e0f5e898d8fe
hamzatamry <58271202+hamzatamry@users.noreply.github.com>
2021-03-10 16:14:14 +0100

    Merge problem

--- main.c
+++ main.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include "semantique_syntaxique.h"
 
-
-
 int main()
 {
     ouvrir_fichier("..\\analyseur_lexical\\tests\\output\\out_1");
     Sym_Suiv();
     PROGRAM();
-    if (good)                                             // variale globale indiquant si l'analyseur syntaxique valide ou pas le fichier
+    if (good)       
+    {
         printf("BRAVO: le programme est correcte!!!");
+    }                                     
     else 
+    {
         printf("PAS BRAVO: fin de programme erronee!!!!");
+    }
     return 0;
 }
